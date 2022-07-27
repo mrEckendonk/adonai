af92b005884b3f623b6bad8eb4bc06de870bf0c0
Mike van Eckendonk <mike.e@vecks.com.br>
2022-07-27 18:05:26 -0300

    Small changes - Added Linters

--- src/components/App.js
+++ src/components/App.js
@@ -1,19 +1,13 @@
-import "../styles/index.scss";
+import '../styles/index.scss';
 
-const App = () => {
-  return (
-    <>
-      
-      <main>
-        <div class="typewriter">
-          <h1>This domain is for sale...   
-          </h1>
-          </div>
-          
-        
-      </main>
-    </>
-  );
-};
+const App = () => (
+  <>
+    <main>
+      <div className="typewriter">
+        <h1>This domain is for sale...</h1>
+      </div>
+    </main>
+  </>
+);
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
-import { render } from "react-dom";
+import { render } from 'react-dom';
 
-import App from "./components/App";
+import App from './components/App';
 
-render(<App />, document.getElementById("root"));
+render(<App />, document.getElementById('root'));
